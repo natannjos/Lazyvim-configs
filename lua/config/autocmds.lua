1116d4a6e588b9cd6b3f9f25734901b36efcba9b
Natan Oliveira <natannjos@gmail.com>
2024-02-06 14:43:54 -0300

    Add colorscheme and options

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,26 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Create an autocommand group named "AutoSaveOnFocusLost"
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+-- Add an autocommand to save all buffers when focus is lost
+vim.api.nvim_create_autocmd("FocusLost", {
+  group = augroup("AutoSaveOnFocusLost"),
+  callback = function()
+    vim.cmd("wa") -- wa stands for 'write all'
+  end,
+  pattern = "*",
+})
+
+-- Add an autocommand to save all buffers when focus is lost
+vim.api.nvim_create_autocmd("FocusGained", {
+  group = augroup("AutoSaveOnFocusGained"),
+  callback = function()
+    vim.cmd("wa") -- wa stands for 'write all'
+  end,
+  pattern = "*",
+})
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -136,7 +136,7 @@ return {
         -- show the '~' characters after the end of buffers
         show_end_of_buffer = true, -- default false
         -- use transparent background
-        transparent_bg = false, -- default false
+        transparent_bg = true, -- default false
         -- set custom lualine background color
         lualine_bg_color = "#44475a", -- default nil
         -- set italic comment
@@ -165,13 +165,13 @@ return {
       require("onedark").setup({
         -- Main options --
         style = "deep", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-        transparent = false, -- Show/hide background
+        transparent = true, -- Show/hide background
         term_colors = true, -- Change terminal color as per the selected theme style
         ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
         cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
 
         -- toggle theme style ---
-        toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+        toggle_style_key = "<leader>ts", -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
         toggle_style_list = { "dark", "darker", "cool", "deep", "warm", "warmer", "light" }, -- List of styles to toggle between
 
         -- Change code style ---
@@ -179,15 +179,15 @@ return {
         -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
         code_style = {
           comments = "italic",
-          keywords = "none",
-          functions = "none",
+          keywords = "bold",
+          functions = "italic,bold",
           strings = "none",
           variables = "none",
         },
 
         -- Lualine options --
         lualine = {
-          transparent = false, -- lualine center bar transparency
+          transparent = true, -- lualine center bar transparency
         },
 
         -- Custom Highlights --
@@ -206,7 +206,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "dracula",
+      colorscheme = "onedark",
       background_colour = "#282828",
     },
   },
