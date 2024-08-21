97becc417702145af58966cbbdd48599948d86cc
Natan Oliveira <natannjos@gmail.com>
2024-08-21 13:16:40 -0300

    Add dropbar plugin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -120,7 +120,7 @@ return {
         -- show the '~' characters after the end of buffers
         show_end_of_buffer = true, -- default false
         -- use transparent background
-        transparent_bg = false, -- default false
+        transparent_bg = true, -- default false
         -- set custom lualine background color
         lualine_bg_color = "#44475a", -- default nil
         -- set italic comment
@@ -205,7 +205,7 @@ return {
     config = function()
       require("cyberdream").setup({
         -- Enable transparent background
-        transparent = false,
+        transparent = true,
 
         -- Enable italics comments
         italic_comments = true,
