1010e0b8eed8d8f638af55cd3941e0ee6f4383b7
Natan Oliveira <natannjos@gmail.com>
2024-10-17 14:22:21 -0300

    Fix deno configs

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -94,3 +94,20 @@ require("telescope").setup({
     },
   },
 })
+
+local nvim_lsp = require("lspconfig")
+nvim_lsp.denols.setup({
+  on_attach = function(client, bufnr)
+    -- Your on_attach function here
+  end,
+  root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc"),
+
+  settings = {
+    deno = {
+      enable = true,
+      lint = true,
+      unstable = true,
+      root_dir = vim.fn.getcwd(),
+    },
+  },
+})
